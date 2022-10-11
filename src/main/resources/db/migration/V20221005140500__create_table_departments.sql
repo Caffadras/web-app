e36fa38bd0056ffcb73e6b9b06dc80fe1d519474
Semion <senya.dubik@gmail.com>
2022-10-11 18:15:06 +0300

    Update flyway files to add sequences

--- src/main/resources/db/migration/V20221005140500__create_table_departments.sql
+++ src/main/resources/db/migration/V20221005140500__create_table_departments.sql
@@ -5,6 +5,8 @@ CREATE TABLE departments
     location_name   VARCHAR2(30) NOT NULL
 );
 
+CREATE SEQUENCE departments_seq  MINVALUE 1 MAXVALUE 9999 INCREMENT BY 1 START WITH 10;
+
 COMMENT ON TABLE departments IS 'Departments table that shows details of departments where employees work.';
 COMMENT ON COLUMN departments.department_id IS 'Primary key. Department id in the database';
 COMMENT ON COLUMN departments.department_name IS 'Name of the department. A not null column.';
